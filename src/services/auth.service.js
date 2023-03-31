c2b7e79c2464b079e6b5134cd5b0b09e3b42332e
datdev2409 <datdev2409@gmail.com>
2023-03-31 16:02:05 +0700

    login route

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcrypt');
-const { BadRequestError } = require('../core/error.reponse');
+const { BadRequestError, NotFoundError } = require('../core/error.reponse');
 const User = require('../models/user.model');
-
+const { createUserToken } = require('../utils/token');
 
 class AuthService {
-  static async register({name, email, password}) {
+  static async register({ name, email, password }) {
     // Check user exists
     const user = await User.findOne({ email }).lean();
     if (user) {
@@ -12,13 +12,26 @@ class AuthService {
     }
 
     const passwordHash = await bcrypt.hash(password, 10);
-    const newUser = await User.create({ name, email, password: passwordHash });
-    return newUser
-    // res.json({ message: 'Register Route' });
+    await User.create({ name, email, password: passwordHash });
+
+    const userToken = createUserToken({ name, email });
+    return { name, email, token: userToken };
   }
 
-  static login(req, res) {
-    res.json({ message: 'Login route' });
+  static async login({ email, password }) {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new NotFoundError('Email does not found');
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      throw new BadRequestError('Wrong email or password');
+    }
+
+    const { name } = user;
+    const userToken = createUserToken({ name, email });
+    return { name, email, token: userToken }
   }
 }
 
