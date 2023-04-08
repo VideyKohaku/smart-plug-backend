6b6d12bac1340e30d91416aaad839937dce6dda3
edohoangt <descience.thh10@gmail.com>
2023-04-09 05:41:54 +0700

    fix minor bugs and add dev data

--- src/services/automation.service.js
+++ src/services/automation.service.js
@@ -1,76 +1,81 @@
-const Automation = require("../models/automation.model");
+const Automation = require('../models/automation.model');
 const { BadRequestError } = require('../core/error.reponse');
 const pickFields = require('../utils/pickFields');
 
 class AutomationService {
-    static async _format(automation) {
-        const fields = ["_id", "name", "user"];
-        return pickFields(automation, fields);
+  static async _format(automation) {
+    const fields = ['_id', 'name', 'user'];
+    return pickFields(automation, fields);
+  }
+
+  static async _getAutomations(query) {
+    const automations = await Automation.find(query).lean();
+    return automations;
+  }
+
+  static async _formatList(automations) {
+    return automations.map((automation) => {
+      return AutomationService._format(automation);
+    });
+  }
+
+  static async getAllAutomations() {
+    const automations = await AutomationService._getAutomations({});
+    const formatAutomations = await Promise.all(
+      await AutomationService._formatList(automations)
+    );
+    return {
+      count: automations.length,
+      automations: formatAutomations,
+    };
+  }
+
+  static async getAllAutomationsByUser({ userId }) {
+    const automations = await AutomationService._getAutomations({
+      user: userId,
+    });
+    const formatAutomations = await Promise.all(
+      await AutomationService._formatList(automations)
+    );
+    return {
+      count: automations.length,
+      automations: formatAutomations,
+    };
+  }
+
+  static async getAutomation(id) {
+    const automation = await Automation.findById(id);
+    console.log(automation);
+    if (!automation) {
+      throw new BadRequestError("Automation's ID not found");
     }
 
-    static async _getAutomations(query) {
-        const automations = await Automation.find(query).lean();
-        return automations
-    }
+    return automation;
+  }
 
-    static async _formatList(automations) {
-        return automations.map((automation) => {
-            return AutomationService._format(automation)
-        });
-    }
+  static async createAutomation({ name, user, actions, time, repeats }) {
+    console.log(name, user, actions);
+    const automation = await Automation.create({
+      name,
+      user,
+      actions,
+      time,
+      repeats,
+    });
+    return automation;
+  }
 
-    static async getAllAutomations() {
-        const automations = await AutomationService._getAutomations({});
-        const formatAutomations = await Promise.all(await AutomationService._formatList(automations)) 
-        return {
-            count: automations.length,
-            automations: formatAutomations
-        };
-    }
+  static async deleteAutomation(id) {
+    await Automation.findByIdAndDelete(id);
+    return;
+  }
 
-    static async getAllAutomationsByUser({userId}) {
-        const automations = await AutomationService._getAutomations({userId});
-        const formatAutomations = await Promise.all(await AutomationService._formatList(automations)) 
-        return {
-            count: automations.length,
-            automations: formatAutomations
-        };
-    }
-
-
-    static async getAutomation(id) {
-        const automation = await Automation.findById(id);
-        console.log(automation);
-        if (!automation) {
-            throw new BadRequestError("Automation's ID not found");
-        }
-
-        return automation;
-    }
-
-    static async createAutomation({ name, user, actions, time, repeats }) {
-        console.log(name, user, actions);
-        const automation = await Automation.create({
-            name,
-            user,
-            actions,
-            time,
-            repeats,
-        });
-        return automation;
-    }
-
-    static async deleteAutomation(id) {
-        await Automation.findByIdAndDelete(id);
-        return;
-    }
-
-    static async updateAutomation(id, data) {
-        const automation = await Automation.findByIdAndUpdate(id, data, {
-            new: true,
-        });
-        return automation;
-    }
+  static async updateAutomation(id, data) {
+    const automation = await Automation.findByIdAndUpdate(id, data, {
+      new: true,
+    });
+    return automation;
+  }
 }
 
 module.exports = AutomationService;
